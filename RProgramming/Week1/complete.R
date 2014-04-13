73d3d193ee413b9d79b4505ab7d1e0621f212c00
Brennan Hildebrand <brenmh@umich.edu>
2014-04-12 20:17:31 -0400

    Added the complete function.

---
+++ RProgramming/Week1/complete.R
@@ -0,0 +1,38 @@
+
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+
+  # create a vector of the filenames for the monitors based on directory
+  # and the ID vector
+  datafiles <- paste0(paste(directory, sprintf("%03d", id), sep = "/"), ".csv")
+
+  # load all the files, rbind'ing as we go and assigne to poldata
+  poldata <- do.call("rbind", lapply(datafiles, function(fn) data.frame(Filename=fn, read.csv(fn))))
+
+  # get the complete cases
+  cc <- poldata[complete.cases(poldata), ]
+
+  # generate a frequency table on ID
+  tab <- table(cc$ID)
+
+  # pivot and name
+  comp_freq <- cbind(as.integer(names(tab)), tab)
+  colnames(comp_freq) <- c("id", "nobs")
+  rownames(comp_freq) <- 1:length(id)
+
+  return(comp_freq)
+
+}
+
